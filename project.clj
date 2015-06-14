e91e000891a507b5983e7f6653a309240fbf5f8c
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-14 11:44:18 -0700

    setup midje with failing dummy test

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject sieve-of-eratosthenes "0.1.0-SNAPSHOT"
   :description "a toy prime number project... in clojure"
   :license {:name "The MIT License"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  :plugins [[lein-midje "3.1.3"]]
+  :test-paths ["test"])
+
--- test/sieve_of_eratosthenes/core_test.clj
+++ test/sieve_of_eratosthenes/core_test.clj
@@ -1,7 +1,7 @@
 (ns sieve-of-eratosthenes.core-test
-  (:require [clojure.test :refer :all]
-            [sieve-of-eratosthenes.core :refer :all]))
+  (:use midje.sweet)
+  (:require [sieve-of-eratosthenes.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(fact "first test"
+  (+ 1 1) => 1)
+
