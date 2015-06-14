4041d4d0264b5616469d97940611cd83ae0b8b5e
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-14 12:11:25 -0700

    fix name mismatch in test

--- test/sieve_of_eratosthenes/core_test.clj
+++ test/sieve_of_eratosthenes/core_test.clj
@@ -2,15 +2,15 @@
   (:use midje.sweet)
   (:require [sieve-of-eratosthenes.core :refer :all]))
 
-(fact "make-range returns empty when n < 2"
+(fact "make-inclusive-range returns empty when n < 2"
       (let [result (make-inclusive-range 1)]
         result => empty?))
 
-(fact "make-range returns 2 when n = 2"
+(fact "make-inclusive-range returns 2 when n = 2"
       (let [result (make-inclusive-range 2)]
         result => (just '(2))))
 
-(fact "make-range returns 2 through n when n > 2"
+(fact "make-inclusive-range returns 2 through n when n > 2"
       (let [result (make-inclusive-range 4)]
         result => (just [2 3 4])))
 
