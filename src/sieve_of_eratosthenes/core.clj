46c2c5f0d1bd15b53f2c9758a5f52b348cb71cf2
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-14 12:09:00 -0700

    create make-range function

--- src/sieve_of_eratosthenes/core.clj
+++ src/sieve_of_eratosthenes/core.clj
@@ -1,6 +1,5 @@
 (ns sieve-of-eratosthenes.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-inclusive-range [n]
+  (range 2 (+ n 1)))
+
--- test/sieve_of_eratosthenes/core_test.clj
+++ test/sieve_of_eratosthenes/core_test.clj
@@ -2,6 +2,15 @@
   (:use midje.sweet)
   (:require [sieve-of-eratosthenes.core :refer :all]))
 
-(fact "first test"
-  (+ 1 1) => 1)
+(fact "make-range returns empty when n < 2"
+      (let [result (make-inclusive-range 1)]
+        result => empty?))
+
+(fact "make-range returns 2 when n = 2"
+      (let [result (make-inclusive-range 2)]
+        result => (just '(2))))
+
+(fact "make-range returns 2 through n when n > 2"
+      (let [result (make-inclusive-range 4)]
+        result => (just [2 3 4])))
 
