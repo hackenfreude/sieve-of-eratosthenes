9bf0a618700eb5dca9e80c0dbe5f65fb216ea65e
hackenfreude <hackenfreude@users.noreply.github.com>
2015-06-14 12:47:50 -0700

    create get-multiples function

--- src/sieve_of_eratosthenes/core.clj
+++ src/sieve_of_eratosthenes/core.clj
@@ -1,5 +1,8 @@
-(ns sieve-of-eratosthenes.core)
+contains (ns sieve-of-eratosthenes.core)
 
 (defn make-inclusive-range [n]
   (range 2 (+ n 1)))
+
+(defn get-multiples-through [p n]
+  (filter (fn [x] (= 0 (mod x p))) (range 1 (+ n 1))))
 
--- test/sieve_of_eratosthenes/core_test.clj
+++ test/sieve_of_eratosthenes/core_test.clj
@@ -14,3 +14,11 @@
       (let [result (make-inclusive-range 4)]
         result => (just [2 3 4])))
 
+(fact "get-multiples-through returns 1 through 10 when p=1 and n=10"
+      (let [result (get-multiples-through 1 10)]
+        result => (just [1 2 3 4 5 6 7 8 9 10])))
+
+(fact "get-multiples-through returns 5, 10, 15 when p=5 and n=17"
+      (let [result (get-multiples-through 5 17)]
+        result => (just [5 10 15])))
+
