469bb8e86bfcae45973441da52d13da88e7741c7
hackenfreude <hackenfreude@users.noreply.github.com>
2016-01-11 21:22:17 -0800

    commit work left hanging

--- src/sieve_of_eratosthenes/core.clj
+++ src/sieve_of_eratosthenes/core.clj
@@ -1,8 +1,18 @@
-(ns sieve-of-eratosthenes.core)
+(ns sieve-of-eratosthenes.core
+  (:use clojure.set))
 
 (defn make-inclusive-range [n]
   (range 2 (+ n 1)))
 
-(defn get-multiples-through [p n]
-  (filter (fn [x] (= 0 (mod x p))) (range 1 (+ n 1))))
+(defn find-multiples-of [p inbound]
+  (filter (fn [x] (= 0 (mod x p))) inbound))
+
+(defn eliminate-multiples [inbound]
+  (let [lowest (apply min inbound)
+        multiples (find-multiples-of lowest inbound)]
+    doall difference '(3 5 7 9 11 13 15) '(3 6 9 12 15)))
+
+
+;(defn find-primes-through [x]
+;  (make-inclusive-range x))
 
--- test/sieve_of_eratosthenes/core_test.clj
+++ test/sieve_of_eratosthenes/core_test.clj
@@ -8,17 +8,28 @@
 
 (fact "make-inclusive-range returns 2 when n = 2"
       (let [result (make-inclusive-range 2)]
-        result => (just '(2))))
+        result => (just [2])))
 
 (fact "make-inclusive-range returns 2 through n when n > 2"
       (let [result (make-inclusive-range 4)]
         result => (just [2 3 4])))
 
-(fact "get-multiples-through returns 1 through 10 when p=1 and n=10"
-      (let [result (get-multiples-through 1 10)]
-        result => (just [1 2 3 4 5 6 7 8 9 10])))
+(fact "find-multiples-of returns 2 through 10 when p=1 and n=10"
+      (let [inbound (make-inclusive-range 10)
+            result (find-multiples-of 1 inbound)]
+        result => (just [2 3 4 5 6 7 8 9 10])))
 
-(fact "get-multiples-through returns 5, 10, 15 when p=5 and n=17"
-      (let [result (get-multiples-through 5 17)]
+(fact "find-multiples-of returns 5, 10, 15 when p=5 and n=17"
+      (let [inbound (make-inclusive-range 17)
+            result (find-multiples-of 5 inbound)]
         result => (just [5 10 15])))
+
+(fact "find-candidates returns 5,7,11,13 when passed 3,5,7,9,11,13,15"
+      (let [candidates (range 3 16 2)
+            result (eliminate-multiples candidates)]
+        result => (just [5 7 11 13])))
+
+;(fact "find-primes-through 3 returns 2, 3"
+;      (let [result (find-primes-through 3)]
+;        result => (just [2 3])))
 
